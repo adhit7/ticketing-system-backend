6ee0303e684dc9669789275ecc265cb27a3a35d9
adhit7 <adhithyanalan@gmail.com>
2024-04-20 19:29:17 +0530

    deployment changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
