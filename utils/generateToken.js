6937afac9c875f5bb51078f4b21355f986975d7b
adhit7 <adhithyanalan@gmail.com>
2024-04-20 19:56:45 +0530

    local branch changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,9 +7,9 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
-    httpOnly: false,
-    secure: true, // We have use secure cookies in production
-    sameSite: 'none', // Prevent CSRF attacks
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // We have use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
