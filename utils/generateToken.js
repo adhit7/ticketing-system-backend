2c6cd48da70e1fa0c3974310d710752c60dd87a0
Adhithyan S <72742240+adhit7@users.noreply.github.com>
2024-08-08 08:41:10 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,6 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: true, // We have use secure cookies in production
+    sameSite: 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
